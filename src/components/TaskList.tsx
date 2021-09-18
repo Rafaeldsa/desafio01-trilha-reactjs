2528a3651adbeb5723a3d7da81a707e9870d1cd9
Rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2021-09-18 12:06:54 -0300

    :tada: desafio completo

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,39 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  function getRandomIntInclusive(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle !== '') {
+      const id = getRandomIntInclusive(1,10000000);
+      const newTask = {
+        id,
+        title: newTaskTitle,
+        isComplete: false
+      }
+      const newTasks = [...tasks, newTask];
+      setTasks(newTasks);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
